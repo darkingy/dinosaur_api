5eebd82b25ee53aab0f6c38242560eff29a0b236
Jiwoo Heo <tmvjswl7890@naver.com>
2024-03-28 11:23:27 +0900

    dinosaur uri changed

--- src/main/java/com/example/dinosaurpark/dinosaur/DinosaurController.java
+++ src/main/java/com/example/dinosaurpark/dinosaur/DinosaurController.java
@@ -10,36 +10,36 @@ import lombok.RequiredArgsConstructor;
 @CrossOrigin
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/dinosaur")
 public class DinosaurController {
     
     private final DinosaurService dinosaurService;
 
-    @GetMapping(value = "/dinolist", produces = "application/json")
+    @GetMapping(produces = "application/json")
     public ResponseEntity<List<Dinosaur>> getAllDinosaurs() {
         List<Dinosaur> dinosaurs = dinosaurService.getList();
         return new ResponseEntity<>(dinosaurs, HttpStatus.OK);
     }
 
-    @GetMapping("/dinolist/{dinosaurId}")
+    @GetMapping("/{dinosaurId}")
     public ResponseEntity<Dinosaur> getDinosaurById(@PathVariable("dinosaurId") Integer id) {
         Dinosaur dinosaur = dinosaurService.getDinosaurById(id);
         return new ResponseEntity<>(dinosaur, HttpStatus.OK);
     }
     
-    @PostMapping(value = "/dinolist", consumes = "application/json", produces = "application/json")
+    @PostMapping(consumes = "application/json", produces = "application/json")
     public ResponseEntity<Dinosaur> addDinosaur(@RequestBody Dinosaur dinosaur) {
         Dinosaur savedDinosaur = dinosaurService.saveDinosaur(dinosaur);
         return new ResponseEntity<>(savedDinosaur, HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/dinolist/{dinosaurId}")
+    @DeleteMapping("{dinosaurId}")
     public ResponseEntity<?> deleteDinosaur(@PathVariable("dinosaurId") Integer id) {
         dinosaurService.deleteDinosaur(id);
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/dinolist/{dinosaurId}")
+    @PutMapping("{dinosaurId}")
     public ResponseEntity<Dinosaur> updateDinosaur(@PathVariable("dinosaurId") Integer id, @RequestBody Dinosaur updatedDinosaur) {
         Dinosaur dinosaur = dinosaurService.updateDinosaur(id, updatedDinosaur);
         return ResponseEntity.ok().body(dinosaur);
